b2cb2e6c4581628a44a844593a89e73335d50a32
newQuery <wilmshorst.tom@gmail.com>
2019-08-02 09:54:10 +0200

    FT/module-customer Init Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/customer-backend', { useNewUrlParser: true }),
+    CustomerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
